d3e5eebeeebe43ee4a88f7c664d1f91fdeb1c16e
Cesar Lara <cesar.lara@yale.edu>
2020-05-22 04:43:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.select do |element|
      element = valid_calls[0]
-     element 
+     element
   end
 end
